aa4dca5ef63997963cd7dd06197b9320d8307f79
R-PG22 <tsukisigure1225@gmail.com>
2026-01-19 18:32:14 +0900

    Nucleoにシリアル通信できるようになった

--- src/joy/src/listener.cpp
+++ src/joy/src/listener.cpp
@@ -1,7 +1,11 @@
 #include <memory>
+#include <fstream>
+#include <iostream>
 #include "rclcpp/rclcpp.hpp"
 #include "joy/joy.hpp"
 #include <sensor_msgs/msg/joy.hpp>
+
+std::ofstream ofs;
 
 class Listener : public rclcpp::Node
 {
@@ -11,7 +15,15 @@ public:
   {
     auto joy_callback =
       [this](std::shared_ptr<sensor_msgs::msg::Joy> msg) -> void {
-        RCLCPP_INFO(this->get_logger(), "\naxes:\n%.2f\n%.2f\n%.2f\n%.2f\n%.2f\n%.2f\n%.2f\n%.2f\nbuttons:\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d", msg->axes[0],msg->axes[1],msg->axes[2],msg->axes[3],msg->axes[4],msg->axes[5],msg->axes[6],msg->axes[7],msg->buttons[0],msg->buttons[1],msg->buttons[2],msg->buttons[3],msg->buttons[4],msg->buttons[5],msg->buttons[6],msg->buttons[7],msg->buttons[8],msg->buttons[9],msg->buttons[10],msg->buttons[11],msg->buttons[12]);
+        RCLCPP_INFO(this->get_logger(), "\nbuttons:\n%d\n%d\n%d\n%d", msg->buttons[0],msg->buttons[1],msg->buttons[2],msg->buttons[3]);
+        if (ofs) {
+          if (msg->buttons[0]) {ofs << "a"; ofs.flush();}
+          if (msg->buttons[1]) {ofs << "b"; ofs.flush();}
+          if (msg->buttons[2]) {ofs << "c"; ofs.flush();}
+          if (msg->buttons[3]) {ofs << "d"; ofs.flush();}
+        }else{
+          RCLCPP_ERROR(this->get_logger(), "File open error.");
+        }
       };
     sub_ = this->create_subscription<sensor_msgs::msg::Joy>("joy", 10, joy_callback);
   }
@@ -21,8 +33,10 @@ public:
 
 int main(int argc, char * argv[])
 {
+  ofs.open("/dev/ttyACM0");
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<Listener>());
   rclcpp::shutdown();
+  ofs.close();
   return 0;
 }
