bb9b22874a8c81a91e44feef0a51fe1bfaf777e9
R-PG22 <tsukisigure1225@gmail.com>
2026-03-03 12:32:02 +0900

    boudrateを明示的に115200にした。通信頻度を50msに制限した。

--- src/joy/src/listener.cpp
+++ src/joy/src/listener.cpp
@@ -1,11 +1,13 @@
 #include <memory>
 #include <fstream>
 #include <iostream>
+#include <termios.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 #include "rclcpp/rclcpp.hpp"
 #include "joy/joy.hpp"
 #include <sensor_msgs/msg/joy.hpp>
-
-std::ofstream ofs;
 
 class Listener : public rclcpp::Node
 {
@@ -13,50 +15,91 @@ public:
   Listener()
   : Node("listener")
   {
+    fd = open("/dev/ttyACM0", O_RDWR | O_NOCTTY);
+    struct termios tty;
+    tcgetattr(fd, &tty);
+    cfmakeraw(&tty);
+    cfsetospeed(&tty, B115200);
+    tcsetattr(fd, TCSANOW, &tty);
+
     auto joy_callback =
-      [this](std::shared_ptr<sensor_msgs::msg::Joy> msg) -> void {
-        RCLCPP_INFO(this->get_logger(), "\naxes:\n%.2f\n%.2f\n%.2f\n%.2f\n%.2f\n%.2f\n%.2f\n%.2f\nbuttons:\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d", msg->axes[0],msg->axes[1],msg->axes[2],msg->axes[3],msg->axes[4],msg->axes[5],msg->axes[6],msg->axes[7],msg->buttons[0],msg->buttons[1],msg->buttons[2],msg->buttons[3],msg->buttons[4],msg->buttons[5],msg->buttons[6],msg->buttons[7],msg->buttons[8],msg->buttons[9],msg->buttons[10],msg->buttons[11],msg->buttons[12]);
-        if (ofs) {
-          ofs << "n";
-          ofs << ":" << msg->axes[0];
-          ofs << ":" << msg->axes[1];
-          ofs << ":" << msg->axes[3];
-          ofs << ":" << msg->axes[4];
-          ofs << "|" << (msg->buttons[4] ? "L1:p" : "L1:no_p");
-          ofs << "|" << (msg->buttons[6] ? "L2:p" : "L2:no_p");
-          ofs << "|" << (msg->buttons[11] ? "L3:p" : "L3:no_p");
-          ofs << "|" << (msg->buttons[5] ? "R1:p" : "R1:no_p");
-          ofs << "|" << (msg->buttons[7] ? "R2:p" : "R2:no_p");
-          ofs << "|" << (msg->buttons[12] ? "R3:p" : "R3:no_p");
-          ofs << "|" << (msg->buttons[10] ? "PS:p" : "PS:no_p");
-          ofs << "|" << (msg->buttons[8] ? "SH:p" : "SH:no_p");
-          ofs << "|" << (msg->buttons[9] ? "OP:p" : "OP:no_p");
-          ofs << "|" << (msg->axes[7]==1.0f ? "u:p" : "u:no_p");
-          ofs << "|" << (msg->axes[7]==-1.0f ? "d:p" : "d:no_p");
-          ofs << "|" << (msg->axes[6]==1.0f ? "l:p" : "l:no_p");
-          ofs << "|" << (msg->axes[6]==-1.0f ? "r:p" : "r:no_p");
-          ofs << "|" << (msg->buttons[1] ? "ci:p" : "ci:no_p");
-          ofs << "|" << (msg->buttons[0] ? "cr:p" : "cr:no_p");
-          ofs << "|" << (msg->buttons[2] ? "tri:p" : "tri:no_p");
-          ofs << "|" << (msg->buttons[3] ? "sq:p" : "sq:no_p");
-          ofs << "|";
-          ofs.flush();
+      [this](const std::shared_ptr<sensor_msgs::msg::Joy> msg) -> void{
+      last_msg = msg;
+      };
+    auto timer_callback =
+      [this]() -> void{
+        RCLCPP_INFO(this->get_logger(), "\naxes:\n%.2f\n%.2f\n%.2f\n%.2f\n%.2f\n%.2f\n%.2f\n%.2f\nbuttons:\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d", last_msg->axes[0],last_msg->axes[1],last_msg->axes[2],last_msg->axes[3],last_msg->axes[4],last_msg->axes[5],last_msg->axes[6],last_msg->axes[7],last_msg->buttons[0],last_msg->buttons[1],last_msg->buttons[2],last_msg->buttons[3],last_msg->buttons[4],last_msg->buttons[5],last_msg->buttons[6],last_msg->buttons[7],last_msg->buttons[8],last_msg->buttons[9],last_msg->buttons[10],last_msg->buttons[11],last_msg->buttons[12]);
+        if (fd == 3) {
+          send_msg += "n";
+          send_msg += ":";
+          send_msg += last_msg->axes[0];
+          send_msg += ":";
+          send_msg += last_msg->axes[1];
+          send_msg += ":";
+          send_msg += last_msg->axes[3];
+          send_msg += ":";
+          send_msg += last_msg->axes[4]; 
+          send_msg += "|";
+          send_msg += (last_msg->buttons[4] ? "L1:p" : "L1:no_p");
+          send_msg += "|";
+          send_msg += (last_msg->buttons[6] ? "L2:p" : "L2:no_p");
+          send_msg += "|";
+          send_msg += (last_msg->buttons[11] ? "L3:p" : "L3:no_p");
+          send_msg += "|";
+          send_msg += (last_msg->buttons[5] ? "R1:p" : "R1:no_p");
+          send_msg += "|";
+          send_msg += (last_msg->buttons[7] ? "R2:p" : "R2:no_p");
+          send_msg += "|";
+          send_msg += (last_msg->buttons[12] ? "R3:p" : "R3:no_p");
+          send_msg += "|";
+          send_msg += (last_msg->buttons[10] ? "PS:p" : "PS:no_p");
+          send_msg += "|";
+          send_msg += (last_msg->buttons[8] ? "SH:p" : "SH:no_p");
+          send_msg += "|";
+          send_msg += (last_msg->buttons[9] ? "OP:p" : "OP:no_p");
+          send_msg += "|";
+          send_msg += (last_msg->axes[7]==1.0f ? "u:p" : "u:no_p");
+          send_msg += "|";
+          send_msg += (last_msg->axes[7]==-1.0f ? "d:p" : "d:no_p");
+          send_msg += "|";
+          send_msg += (last_msg->axes[6]==1.0f ? "l:p" : "l:no_p");
+          send_msg += "|";
+          send_msg += (last_msg->axes[6]==-1.0f ? "r:p" : "r:no_p");
+          send_msg += "|";
+          send_msg += (last_msg->buttons[1] ? "ci:p" : "ci:no_p");
+          send_msg += "|";
+          send_msg += (last_msg->buttons[0] ? "cr:p" : "cr:no_p");
+          send_msg += "|";
+          send_msg += (last_msg->buttons[2] ? "tri:p" : "tri:no_p");
+          send_msg += "|";
+          send_msg += (last_msg->buttons[3] ? "sq:p" : "sq:no_p");
+          send_msg += "|";
+          // ofs.flush();
+          write(fd, send_msg.c_str(), strlen(send_msg.c_str()));
         }else{
-          RCLCPP_ERROR(this->get_logger(), "File open error.");
+          RCLCPP_ERROR(this->get_logger(), "Can't open file.");
         }
       };
+    
     sub_ = this->create_subscription<sensor_msgs::msg::Joy>("joy", 10, joy_callback);
-  }
 
+    using namespace std::chrono_literals;
+    timer_ = this->create_wall_timer(50ms, timer_callback);
+  }
+  ~Listener(){close(fd);}
+
+private:
   rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr sub_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  sensor_msgs::msg::Joy::SharedPtr last_msg;
+  int fd;
+  std::string send_msg = "";
 };
 
 int main(int argc, char * argv[])
 {
-  ofs.open("/dev/ttyACM0");
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<Listener>());
   rclcpp::shutdown();
-  ofs.close();
   return 0;
 }
