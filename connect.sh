cb704dff6e1456ae760bc4b5bded83eda7980bc7
R-PG22 <tsukisigure1225@gmail.com>
2026-07-18 12:10:26 +0900

    feat: 接続用ファイルを追加

---
+++ connect.sh
@@ -0,0 +1,3 @@
+gnome-terminal -- bash -c "source ~/Projects/ros2/joy_test; ros2 run joy joy_node; bash"
+gnome-terminal -- bash -c "cd ~/Projects/ros2/joy_test; . install/setup.bash; ros2 run joy listener; bash"
+
